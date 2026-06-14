62cb3745a59bcd86f2f80a8194a968c9484e6942
nic <nicodemus.landaverde98@gmail.com>
2026-06-14 20:46:17 -0300

    fixed lighting

--- scenes/menus/main_menu.gd
+++ scenes/menus/main_menu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+@onready var _solo_btn: Button = $CenterContainer/VBoxContainer/Solo
+
+func _ready() -> void:
+	_solo_btn.grab_focus()
 
 func _on_solo_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/modes/solo/solo_mode.tscn")
