f117094cfa62e10d324970260397bc5f081b69cd
nic <nicodemus.landaverde98@gmail.com>
2026-06-28 20:35:36 -0300

    compile shaders

--- scenes/menus/main_menu.gd
+++ scenes/menus/main_menu.gd
@@ -5,9 +5,33 @@ extends Control
 @onready var _options_btn: Button     = $SafeArea/MenuGrid/ActionsPanel/Actions/Options
 @onready var _options_popup: Control  = $OptionsPopup
 
+const _WARMUP_SHADERS := [
+	"res://scenes/game/effects/hard_drop_streak.gdshader",
+	"res://scenes/game/effects/line_clear.gdshader",
+	"res://scenes/game/effects/lightning_flash.gdshader",
+	"res://scenes/game/effects/frame_glow.gdshader",
+	"res://scenes/game/effects/glow_outline.gdshader",
+	"res://scenes/game/effects/danger_overlay.gdshader",
+	"res://scenes/game/effects/lightning_zap.gdshader",
+	"res://scenes/game/effects/garbage_cell.gdshader",
+]
+
 func _ready() -> void:
 	_solo_btn.grab_focus()
 	_options_popup.closed.connect(_on_options_closed)
+	_warmup_shaders()
+
+func _warmup_shaders() -> void:
+	var rect := ColorRect.new()
+	rect.size = Vector2(1, 1)
+	rect.position = Vector2(-9999, -9999)
+	add_child(rect)
+	for path in _WARMUP_SHADERS:
+		var mat := ShaderMaterial.new()
+		mat.shader = load(path)
+		rect.material = mat
+		await get_tree().process_frame
+	rect.queue_free()
 
 func _on_solo_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/modes/solo/solo_mode.tscn")
